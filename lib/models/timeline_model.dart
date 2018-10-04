96d17e852ff17abb822da2c423289aa4cb1f248c
kanomdook <kanung.2535@gmail.com>
2018-10-04 11:51:48 +0700

    [add] builder

--- lib/models/timeline_model.dart
+++ lib/models/timeline_model.dart
@@ -2,6 +2,8 @@ class TimelineModel {
   String name;
   String profileImageURL;
   String imageURL;
+  String detail;
+  String time;
 
-  TimelineModel(this.name, this.profileImageURL, this.imageURL);
+  TimelineModel(this.name, this.profileImageURL, this.imageURL, this.detail, this.time);
 }
--- lib/widgets/timeline_list.dart
+++ lib/widgets/timeline_list.dart
@@ -6,96 +6,257 @@ class TimelineList extends StatefulWidget {
 }
 
 class _TimelineListState extends State<TimelineList> {
-
   List<TimelineModel> timelineList = new List<TimelineModel>();
 
   _TimelineListState() {
-    timelineList.add(TimelineModel('VIVO Thailand Official', 'http://db.lnwfile.com/_/db/_raw/qv/qs/cn.png', 'https://selular.id/wp-content/uploads/2018/09/IMG_20180911_092730.jpg'));
-    timelineList.add(TimelineModel('VIVO Thailand Official', 'http://db.lnwfile.com/_/db/_raw/qv/qs/cn.png', 'https://selular.id/wp-content/uploads/2018/09/IMG_20180911_092730.jpg'));
-    timelineList.add(TimelineModel('VIVO Thailand Official', 'http://db.lnwfile.com/_/db/_raw/qv/qs/cn.png', 'https://selular.id/wp-content/uploads/2018/09/IMG_20180911_092730.jpg'));
-    timelineList.add(TimelineModel('VIVO Thailand Official', 'http://db.lnwfile.com/_/db/_raw/qv/qs/cn.png', 'https://selular.id/wp-content/uploads/2018/09/IMG_20180911_092730.jpg'));
-    timelineList.add(TimelineModel('VIVO Thailand Official', 'http://db.lnwfile.com/_/db/_raw/qv/qs/cn.png', 'https://selular.id/wp-content/uploads/2018/09/IMG_20180911_092730.jpg'));
-    timelineList.add(TimelineModel('VIVO Thailand Official', 'http://db.lnwfile.com/_/db/_raw/qv/qs/cn.png', 'https://selular.id/wp-content/uploads/2018/09/IMG_20180911_092730.jpg'));
-    timelineList.add(TimelineModel('VIVO Thailand Official', 'http://db.lnwfile.com/_/db/_raw/qv/qs/cn.png', 'https://selular.id/wp-content/uploads/2018/09/IMG_20180911_092730.jpg'));
-    timelineList.add(TimelineModel('VIVO Thailand Official', 'http://db.lnwfile.com/_/db/_raw/qv/qs/cn.png', 'https://selular.id/wp-content/uploads/2018/09/IMG_20180911_092730.jpg'));
-    timelineList.add(TimelineModel('VIVO Thailand Official', 'http://db.lnwfile.com/_/db/_raw/qv/qs/cn.png', 'https://selular.id/wp-content/uploads/2018/09/IMG_20180911_092730.jpg'));
+    timelineList.add(TimelineModel(
+        'VIVO Thailand Official',
+        'http://db.lnwfile.com/_/db/_raw/qv/qs/cn.png',
+        'https://selular.id/wp-content/uploads/2018/09/IMG_20180911_092730.jpg',
+        'สัมผัสมุมมองใหม่ เรียบหรูกว่าที่เคย โดดเด่นด้วยรูปทรงบางเฉียบบน V11 In-Display Fingerfrint ID ด้วยดีไซน์ แบบ Halo FullView Display จอแสดงผลอัตราส่วน 19.5:9 มาพร้อมกับเทคโนโลยีบนจอแสดงผล Super AMOLED...',
+        '12 นาทีที่แล้ว'
+        ));
+
+        timelineList.add(TimelineModel(
+        'VIVO Thailand Official',
+        'http://db.lnwfile.com/_/db/_raw/qv/qs/cn.png',
+        'https://selular.id/wp-content/uploads/2018/09/IMG_20180911_092730.jpg',
+        'สัมผัสมุมมองใหม่ เรียบหรูกว่าที่เคย โดดเด่นด้วยรูปทรงบางเฉียบบน V11 In-Display Fingerfrint ID ด้วยดีไซน์ แบบ Halo FullView Display จอแสดงผลอัตราส่วน 19.5:9 มาพร้อมกับเทคโนโลยีบนจอแสดงผล Super AMOLED...',
+        '12 นาทีที่แล้ว'
+        ));
+
+        timelineList.add(TimelineModel(
+        'VIVO Thailand Official',
+        'http://db.lnwfile.com/_/db/_raw/qv/qs/cn.png',
+        'https://selular.id/wp-content/uploads/2018/09/IMG_20180911_092730.jpg',
+        'สัมผัสมุมมองใหม่ เรียบหรูกว่าที่เคย โดดเด่นด้วยรูปทรงบางเฉียบบน V11 In-Display Fingerfrint ID ด้วยดีไซน์ แบบ Halo FullView Display จอแสดงผลอัตราส่วน 19.5:9 มาพร้อมกับเทคโนโลยีบนจอแสดงผล Super AMOLED...',
+        '12 นาทีที่แล้ว'
+        ));
+
+        timelineList.add(TimelineModel(
+        'VIVO Thailand Official',
+        'http://db.lnwfile.com/_/db/_raw/qv/qs/cn.png',
+        'https://selular.id/wp-content/uploads/2018/09/IMG_20180911_092730.jpg',
+        'สัมผัสมุมมองใหม่ เรียบหรูกว่าที่เคย โดดเด่นด้วยรูปทรงบางเฉียบบน V11 In-Display Fingerfrint ID ด้วยดีไซน์ แบบ Halo FullView Display จอแสดงผลอัตราส่วน 19.5:9 มาพร้อมกับเทคโนโลยีบนจอแสดงผล Super AMOLED...',
+        '12 นาทีที่แล้ว'
+        ));
+
+        timelineList.add(TimelineModel(
+        'VIVO Thailand Official',
+        'http://db.lnwfile.com/_/db/_raw/qv/qs/cn.png',
+        'https://selular.id/wp-content/uploads/2018/09/IMG_20180911_092730.jpg',
+        'สัมผัสมุมมองใหม่ เรียบหรูกว่าที่เคย โดดเด่นด้วยรูปทรงบางเฉียบบน V11 In-Display Fingerfrint ID ด้วยดีไซน์ แบบ Halo FullView Display จอแสดงผลอัตราส่วน 19.5:9 มาพร้อมกับเทคโนโลยีบนจอแสดงผล Super AMOLED...',
+        '12 นาทีที่แล้ว'
+        ));
+
+        timelineList.add(TimelineModel(
+        'VIVO Thailand Official',
+        'http://db.lnwfile.com/_/db/_raw/qv/qs/cn.png',
+        'https://selular.id/wp-content/uploads/2018/09/IMG_20180911_092730.jpg',
+        'สัมผัสมุมมองใหม่ เรียบหรูกว่าที่เคย โดดเด่นด้วยรูปทรงบางเฉียบบน V11 In-Display Fingerfrint ID ด้วยดีไซน์ แบบ Halo FullView Display จอแสดงผลอัตราส่วน 19.5:9 มาพร้อมกับเทคโนโลยีบนจอแสดงผล Super AMOLED...',
+        '12 นาทีที่แล้ว'
+        ));
+
+        timelineList.add(TimelineModel(
+        'VIVO Thailand Official',
+        'http://db.lnwfile.com/_/db/_raw/qv/qs/cn.png',
+        'https://selular.id/wp-content/uploads/2018/09/IMG_20180911_092730.jpg',
+        'สัมผัสมุมมองใหม่ เรียบหรูกว่าที่เคย โดดเด่นด้วยรูปทรงบางเฉียบบน V11 In-Display Fingerfrint ID ด้วยดีไซน์ แบบ Halo FullView Display จอแสดงผลอัตราส่วน 19.5:9 มาพร้อมกับเทคโนโลยีบนจอแสดงผล Super AMOLED...',
+        '12 นาทีที่แล้ว'
+        ));
+
+        timelineList.add(TimelineModel(
+        'VIVO Thailand Official',
+        'http://db.lnwfile.com/_/db/_raw/qv/qs/cn.png',
+        'https://selular.id/wp-content/uploads/2018/09/IMG_20180911_092730.jpg',
+        'สัมผัสมุมมองใหม่ เรียบหรูกว่าที่เคย โดดเด่นด้วยรูปทรงบางเฉียบบน V11 In-Display Fingerfrint ID ด้วยดีไซน์ แบบ Halo FullView Display จอแสดงผลอัตราส่วน 19.5:9 มาพร้อมกับเทคโนโลยีบนจอแสดงผล Super AMOLED...',
+        '12 นาทีที่แล้ว'
+        ));
   }
-
-
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
+    return ListView.builder(
       scrollDirection: Axis.vertical,
-      children: <Widget>[
-        Column(
-          children: <Widget>[
-            Container(
-              padding: EdgeInsets.all(10.0),
-              child: Row(
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.all(4.0),
-                    child: CircleAvatar(
-                    backgroundImage: NetworkImage('http://db.lnwfile.com/_/db/_raw/qv/qs/cn.png'),
-                  ),
-                  ),
-                  Expanded(
-                    child: Padding(
+      itemCount: timelineList.length,
+      itemBuilder: (context, index) {
+        var timeline = timelineList[index];
+        return Container(
+          child: Column(
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.all(4.0),
+                      child: CircleAvatar(
+                        backgroundImage: NetworkImage(timeline.profileImageURL),
+                      ),
+                    ),
+                    Expanded(
+                        child: Padding(
                       padding: EdgeInsets.all(4.0),
                       child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Container(
+                            padding: const EdgeInsets.only(bottom: 4.0),
+                            child: Text(timeline.name,
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          Text(timeline.time,
+                            style: TextStyle(color: Colors.grey[600]),
+                          ),
+                        ],
+                      ),
+                    )),
+                    Icon(
+                      Icons.more_horiz,
+                      color: Colors.grey,
+                    )
+                  ],
+                ),
+              ),
+              Column(
+                children: <Widget>[
+                  Image.network(timeline.imageURL)
+                ],
+              ),
+              Column(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(timeline.detail,
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  )
+                ],
+              ),
+              Column(
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: <Widget>[
-                        Container(
-                          padding: const EdgeInsets.only(bottom: 4.0),
-                          child: Text('VIVO Thailand Official', style: TextStyle(
-                            fontWeight: FontWeight.bold
-                          ),),
+                        Icon(
+                          Icons.face,
+                          color: Colors.pinkAccent,
+                          size: 30.0,
                         ),
-                        Text('12 นาทีที่แล้ว', style: TextStyle(
-                          color: Colors.grey[600]
+                        Icon(
+                          Icons.chat,
+                          color: Colors.greenAccent,
+                          size: 30.0,
                         ),
+                        Icon(
+                          Icons.share,
+                          color: Colors.blueAccent,
+                          size: 30.0,
                         ),
                       ],
                     ),
-                    )
-                  ),
-                  Icon(Icons.more_horiz, color: Colors.grey,)
+                  )
                 ],
-              ),
-            )
-          ],
-        ),
-        Column(
-          children: <Widget>[
-            Image.network('https://selular.id/wp-content/uploads/2018/09/IMG_20180911_092730.jpg')
-          ],
-        ),
-        Column(
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Text('ให้ทุกการใช้งานสะดวกสบาย และ ปลอดภัยมากยิ่งขึ้นบน V11 In-Display Fingerprint ID ด้วยเทคโนโลยีสแกนนิ้วปลดล็อกตัวเครื่องสมาร์ทโฟนบนหน้าจอจะปรากฏไอคอนกราฟฟิกลายนิ้วมือ เพียงแค่คุณแตะก็จะทำการปลดล็อกได้อย่างรวดเร็วและปลอดภัย...',
-              style: TextStyle(fontWeight: FontWeight.bold),),
-            )
-          ],
-        ),
-        Column(
-          children: <Widget>[
-            Container(
-              padding: EdgeInsets.all(8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Icon(Icons.face, color: Colors.pinkAccent,size: 30.0,),
-                  Icon(Icons.chat, color: Colors.greenAccent,size: 30.0,),
-                  Icon(Icons.share, color: Colors.blueAccent,size: 30.0,),
-                ],
-              ),
-            )
-          ],
-        )
-      ],
+              )
+            ],
+          ),
+        );
+      },
     );
+
+    // return ListView(
+    //   scrollDirection: Axis.vertical,
+    //   children: <Widget>[
+    //     Column(
+    //       children: <Widget>[
+    //         Container(
+    //           padding: EdgeInsets.all(10.0),
+    //           child: Row(
+    //             children: <Widget>[
+    //               Padding(
+    //                 padding: EdgeInsets.all(4.0),
+    //                 child: CircleAvatar(
+    //                   backgroundImage: NetworkImage(
+    //                       'http://db.lnwfile.com/_/db/_raw/qv/qs/cn.png'),
+    //                 ),
+    //               ),
+    //               Expanded(
+    //                   child: Padding(
+    //                 padding: EdgeInsets.all(4.0),
+    //                 child: Column(
+    //                   crossAxisAlignment: CrossAxisAlignment.start,
+    //                   children: <Widget>[
+    //                     Container(
+    //                       padding: const EdgeInsets.only(bottom: 4.0),
+    //                       child: Text(
+    //                         'VIVO Thailand Official',
+    //                         style: TextStyle(fontWeight: FontWeight.bold),
+    //                       ),
+    //                     ),
+    //                     Text(
+    //                       '12 นาทีที่แล้ว',
+    //                       style: TextStyle(color: Colors.grey[600]),
+    //                     ),
+    //                   ],
+    //                 ),
+    //               )),
+    //               Icon(
+    //                 Icons.more_horiz,
+    //                 color: Colors.grey,
+    //               )
+    //             ],
+    //           ),
+    //         )
+    //       ],
+    //     ),
+    //     Column(
+    //       children: <Widget>[
+    //         Image.network(
+    //             'https://selular.id/wp-content/uploads/2018/09/IMG_20180911_092730.jpg')
+    //       ],
+    //     ),
+    //     Column(
+    //       children: <Widget>[
+    //         Padding(
+    //           padding: EdgeInsets.all(8.0),
+    //           child: Text(
+    //             'ให้ทุกการใช้งานสะดวกสบาย และ ปลอดภัยมากยิ่งขึ้นบน V11 In-Display Fingerprint ID ด้วยเทคโนโลยีสแกนนิ้วปลดล็อกตัวเครื่องสมาร์ทโฟนบนหน้าจอจะปรากฏไอคอนกราฟฟิกลายนิ้วมือ เพียงแค่คุณแตะก็จะทำการปลดล็อกได้อย่างรวดเร็วและปลอดภัย...',
+    //             style: TextStyle(fontWeight: FontWeight.bold),
+    //           ),
+    //         )
+    //       ],
+    //     ),
+    //     Column(
+    //       children: <Widget>[
+    //         Container(
+    //           padding: EdgeInsets.all(8.0),
+    //           child: Row(
+    //             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    //             children: <Widget>[
+    //               Icon(
+    //                 Icons.face,
+    //                 color: Colors.pinkAccent,
+    //                 size: 30.0,
+    //               ),
+    //               Icon(
+    //                 Icons.chat,
+    //                 color: Colors.greenAccent,
+    //                 size: 30.0,
+    //               ),
+    //               Icon(
+    //                 Icons.share,
+    //                 color: Colors.blueAccent,
+    //                 size: 30.0,
+    //               ),
+    //             ],
+    //           ),
+    //         )
+    //       ],
+    //     )
+    //   ],
+    // );
   }
 }
