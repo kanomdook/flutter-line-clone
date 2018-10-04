bc5ce91b31ff105eb46113414b13c88318703bc5
kanomdook <kanung.2535@gmail.com>
2018-10-04 10:56:11 +0700

    [edit] bottom navigation

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,11 +6,33 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('ไทม์ไลน์' ,style: TextStyle(
-          color: Colors.white
-        ),),
+        title: Text(
+          'ไทม์ไลน์',
+          style: TextStyle(color: Colors.white),
+        ),
       ),
       body: TimelineList(),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.timeline, color: Colors.black,),
+            title: Text('ไทม์ไลน์' ,style: TextStyle(color: Colors.black),),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search, color: Colors.grey,),
+            title: Text('ค้นหา' ,style: TextStyle(color: Colors.grey),),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications, color: Colors.grey,),
+            title: Text('แจ้งเตือน' ,style: TextStyle(color: Colors.grey),),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person, color: Colors.grey,),
+            title: Text('โปรไฟล์' ,style: TextStyle(color: Colors.grey),),
+          ),
+        ],
+      ),
     );
   }
 }
